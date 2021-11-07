fd635cb0afe4c1567cdcc8320a7c01b2de0d35e3
tatsutei6 <tatsutei6@gmail.com>
2021-11-08 00:21:00 +0900

    thymeleaf form data binding

---
+++ src/main/java/org/tei/form/SessionForm.java
@@ -0,0 +1,12 @@
+package org.tei.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SessionForm {
+    private String sessionInfo;
+}
